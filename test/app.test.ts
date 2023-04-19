1efbe41037148b210ddf00fe6a2998650dd5cc7c
Billy Onjea <vasili.onjea@gmail.com>
2023-04-19 14:45:39 -0400

    cleanup

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,12 +1,3 @@
-import App from '../src/app';
-
-let app: App;
-
-beforeEach(() => {
-  app = new App();
-  app.start();
-});
-
 test('it is expected TODO', () => {
   expect(true).toBe(true);
 });
